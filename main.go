70931db1a9cc2e18cef22978f4ac4050ffe21899
Dragosh Vladimir <s414755@gmail.com>
2024-02-24 16:52:19 +0300

    Благодарность

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/docker", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<h1>Hello from Docker contaiter!\nThis docker-image run on server!</h1>")
+		fmt.Fprintf(w, "<h1>Thenk You, John!!!</h1>")
 	})
 
 	http.ListenAndServe(":8080", nil)
